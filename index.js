0d8408788946ea0fc728bff10ce8f790356e4590
kartikeyGehlot2328 <kartikey.22bcon777@jecrcu.edu.in>
2025-08-10 12:28:47 +0530

    updated listen code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const Routes = require('./Routes/routes.js');
 
-app.listen(8080);
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({ origin:true}));
